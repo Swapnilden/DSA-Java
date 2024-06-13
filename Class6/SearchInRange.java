714ffb46e293d33b1636866d00050d7a90a03998
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2024-06-13 12:51:06 +0530

    Update SearchInRange.java

--- Class6/SearchInRange.java
+++ Class6/SearchInRange.java
@@ -1,5 +1,24 @@
 public class SearchInRange {
   public static void main(String[] args){
+    int[] arr = {18, 12, -7, 3, 14, 28};
+    int target = 3;
     
+  }
+
+  static int linearSearch(int[] arr, int target, int start, int end) {
+    if(arr.length == 0){
+      return -1;
+    }
+    // run a for loop
+    for(int index = start; index <= end; index++){
+      // check for element at every index
+      int element = arr[index];
+      if(element == target){
+        return index;
+      }
+    }
+    // this line is executed if none of the return statements above have executed 
+    // hence the target not found
+    return -1;
   }
 }
