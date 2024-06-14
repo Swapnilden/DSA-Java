502e82474e9664d614eef64ba3a91218341b3460
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2024-06-14 18:51:00 +0530

    Update SearchInRange.java

--- Class6/SearchInRange.java
+++ Class6/SearchInRange.java
@@ -2,7 +2,7 @@ public class SearchInRange {
   public static void main(String[] args){
     int[] arr = {18, 12, -7, 3, 14, 28};
     int target = 3;
-    
+    System.out.println(linearSearch(arr, target, 1, 4));
   }
 
   static int linearSearch(int[] arr, int target, int start, int end) {
